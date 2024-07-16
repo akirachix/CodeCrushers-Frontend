08803a1470741720922a0f13b0302f30de1b6b7b
nyabangWech <nyabangwech@gmail.com>
2024-07-16 22:47:35 +0300

    Added  feature/Subjects

--- app/src/main/java/com/akirachix/xomi_application/subjects.kt
+++ app/src/main/java/com/akirachix/xomi_application/subjects.kt
@@ -1,20 +1,47 @@
 package com.akirachix.xomi_application
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akirachix.xomi_application.databinding.ActivitySubjectsBinding
+
 
 class subjects : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_subjects)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val binding=ActivitySubjectsBinding.inflate(layoutInflater)
+
+            setContentView(binding.root)
+            val button = findViewById<TextView>(R.id.btView1)
+            button.setOnClickListener {
+                val intent = Intent(this,subjects::class.java)
+                startActivity(intent)
+            }
+            val button1 = findViewById<TextView>(R.id.btView2)
+            button1.setOnClickListener {
+                val intent = Intent(this,subjects::class.java)
+                startActivity(intent)
+            }
+            val button2 = findViewById<TextView>(R.id.btView3)
+            button2.setOnClickListener {
+                val intent = Intent(this,subjects::class.java)
+                startActivity(intent)
+            }
+            val button3 = findViewById<TextView>(R.id.btView4)
+            button3.setOnClickListener {
+                val intent = Intent(this,subjects::class.java)
+                startActivity(intent)
+            }
+            val button4 = findViewById<TextView>(R.id.btBack)
+            button4.setOnClickListener {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
-}
